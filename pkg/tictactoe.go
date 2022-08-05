ea6c1cc6f133e8eb6cb1e4a06ae08554aee76ab8
lucianogarciaz <luciano.garcia@typeform.com>
2022-08-05 17:41:26 +0200

    Start implementing not player turn error

--- pkg/tictactoe.go
+++ pkg/tictactoe.go
@@ -1,9 +1,17 @@
 package pkg
 
+import "errors"
+
 const (
 	playerX = "X"
 	playerO = "O"
 )
+
+var ErrNotPlayerTurn = errors.New("not player turn")
+
+type Field struct {
+	X, Y int
+}
 
 type Game struct {
 }
@@ -18,6 +26,10 @@ func NewGame() Game {
 
 func (g *Game) Init() {
 
+}
+
+func (g *Game) Take(player string, field Field) error {
+	return nil
 }
 
 func (g Game) State() string {
